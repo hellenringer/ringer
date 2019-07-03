d53269042f95b28deacd43d272e4dcee740fde9d
hellenringer <helen109618@gmail.com>
2019-07-03 15:23:14 +0400

    рандом

--- database/migrations/2019_07_02_102422_create_groups_table.php
+++ database/migrations/2019_07_02_102422_create_groups_table.php
@@ -17,9 +17,9 @@ class CreateGroupsTable extends Migration
             $table->bigIncrements('id');
             $table->string('title');
             $table->string('description');
-            $table->double('average_math')->nullable()->default(0);
-            $table->double('average_rus')->nullable()->default(0);
-            $table->double('average_eng')->nullable()->default(0);
+            $table->double('average_math');
+            $table->double('average_rus');
+            $table->double('average_eng');
             $table->timestamps();
         });
     }
