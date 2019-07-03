7a2bc45ebd7f2aff6f5f072c56a823e204af20c2
hellenringer <helen109618@gmail.com>
2019-07-03 15:16:25 +0400

    рандомные оценки

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -9,6 +9,9 @@ $factory->define(Groups::class, function (Faker $faker) {
     $faker = \Faker\factory::create('ru_RU');
     return [
         'title' => $faker->city,
-        'description' => $faker->unique()->word
+        'description' => $faker->unique()->word, 
+        'average_math' => rand(2,5), 
+        'average_rus' => rand(2,5), 
+        'average_eng' => rand(2,5),
     ];
 });
